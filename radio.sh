2207bf92a6cb30a9422c7cdf03a58fadb7d91a9c
Mauricio Robayo <archimiro@users.noreply.github.com>
2017-05-06 14:03:28 -0500

    add more radio stations

--- radio.sh
+++ radio.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 realpath="$(realpath $0)"
-radiostations="${realpath%/*}/radio-stations"
-i=0
-while read -r line; do
-    let i++
-    printf "%d) %s\n" $i "$(echo $line | cut -d',' -f1)"
-done < $radiostations
-printf "\nStation number? "
-read station
-stream=$(tail -n+${station} $radiostations | head -n1 | cut -d',' -f2)
-mpv $stream
+radiostations="${realpath%/*}/radiostations"
+
+if [[ "$1" =~ ^http ]]; then
+   printf "Name for '$1': " 
+   read name
+   echo "${name},${1}" >> "$radiostations"
+else
+    i=0
+    while read -r line; do
+        let i++
+        printf "%d) %s\n" $i "$(echo $line | cut -d',' -f1)"
+    done < $radiostations
+    printf "\nStation number? "
+    read station
+    stream=$(tail -n+${station} "$radiostations" | head -n1 | cut -d',' -f2)
+    mpv "$stream"
+fi
